9ec30640b9099598eb09ae3f552f9c50d82dd0bd
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2019-02-09 16:26:18 +0700

    Initialize configs

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"go-member/internal/app"
 	"go-member/internal/pkg/member"
 	"go-member/internal/pkg/pingpong"
 	"log"
@@ -10,11 +11,14 @@ import (
 )
 
 func main() {
-	router := mux.NewRouter()
+	if err := app.InitConfig(); err != nil {
+		panic(err)
+	}
+	log.Println("Initial Config: ", app.CFG)
 
+	router := mux.NewRouter()
 	router.HandleFunc("/ping", pingpong.PingPong).Methods("GET")
 	router.HandleFunc("/ping", pingpong.PingPongPost).Methods("POST")
-
 	router.HandleFunc("/member", member.CreateMemberAccount).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":8050", router))
